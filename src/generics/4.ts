4686620fb1d981c62679b8d424c02480af10f3e9
Bohdana28 <bog.olhowetzcka@gmail.com>
2024-08-15 17:40:09 +0300

    typescript-task

---
+++ src/generics/4.ts
@@ -0,0 +1,16 @@
+type Users = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<Users>) {
+  // Оновлення користувача
+  console.log(initialValues);
+}
+
+createOrUpdateUser({ 
+  email: 'user@mail.com', 
+  password: 'password123' 
+});
